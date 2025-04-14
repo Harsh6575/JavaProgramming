f3ac61e0912338bb1225f8d665421ca0c2fc24e9
“Harsh6575” <harsh.190410107146@gmail.com>
2025-04-14 11:51:09 +0530

    Leetcode 1732 sol prefix sum

---
+++ src/leetcode/LC1732.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC1732 {
+    /*
+        Leetcode 1732. Find the Highest Altitude
+        Prefix Sum
+    */
+
+    static int largestAltitude(int[] gain) {
+        int[] ansA = new int[gain.length+1];
+        ansA[0]=0;
+        for (int i = 0; i < gain.length; i++) {
+            ansA[i+1]=ansA[i]+gain[i];
+        }
+        int ans=Integer.MIN_VALUE;
+        for (int i = 0; i < ansA.length; i++) {
+            if (ansA[i]>ans){
+                ans=ansA[i];
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={-5,1,5,0,-7};
+        System.out.println(largestAltitude(arr));
+    }
+}
