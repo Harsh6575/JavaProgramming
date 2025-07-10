f5b784e150713ce368f96089a8f29a5263559b7a
“Harsh6575” <harsh.190410107146@gmail.com>
2025-07-10 11:59:27 +0530

    problem solvng codeforce 10/07/2025

---
+++ src/CodeForces/OneThreeThirtyFiveA.java
@@ -0,0 +1,16 @@
+package CodeForces;
+
+// Candies and Two Sisters
+
+import java.util.Scanner;
+
+public class OneThreeThirtyFiveA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t=sc.nextInt();
+        while (t-- >0){
+            System.out.println((sc.nextInt()-1)/2);
+        }
+    }
+}
