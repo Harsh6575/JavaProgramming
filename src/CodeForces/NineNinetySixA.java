915cef3b4dfb1781544e3603b649a01d5a4ea88d
“Harsh6575” <harsh.190410107146@gmail.com>
2025-07-11 21:41:31 +0530

    996A Codeforces

---
+++ src/CodeForces/NineNinetySixA.java
@@ -0,0 +1,17 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class NineNinetySixA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] coins = {100, 20, 10, 5, 1};
+        int count = 0;
+        for (int coin : coins) {
+            count += n / coin;
+            n %= coin;
+        }
+        System.out.println(count);
+    }
+}
