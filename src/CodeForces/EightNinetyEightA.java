5b220a35c93c90c36d0dd057a61f1447000b5c43
“Harsh6575” <harsh.190410107146@gmail.com>
2025-07-16 19:45:52 +0530

    898A – Rounding

---
+++ src/CodeForces/EightNinetyEightA.java
@@ -0,0 +1,13 @@
+package CodeForces;
+
+// Rounding
+
+import java.util.Scanner;
+
+public class EightNinetyEightA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(((n + 5) / 10) * 10);
+    }
+}
