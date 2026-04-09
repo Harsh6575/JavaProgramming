ecd4a1bff85873fe9babd2e5bf895d817d1c1623
“Harsh6575” <harsh.190410107146@gmail.com>
2026-04-09 06:49:57 +0530

    Add solution for Leetcode 1903: Largest Odd Number in String

---
+++ src/leetcode/LC1903.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LC1903 {
+    /*
+        Leetcode 1903. Largest Odd Number in String
+    */
+
+    static String largestOddNumber(String num) {
+        for (int i=num.length()-1; i>=0; i--){
+            if ((num.charAt(i)-'0')%2==1) return num.substring(0, i+1);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(largestOddNumber("52"));
+        System.out.println(largestOddNumber("4206"));
+        System.out.println(largestOddNumber("35427"));
+    }
+}
