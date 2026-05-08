36f310abe83609ce6bee30df29e0514a8e9947cc
“Harsh6575” <harsh.190410107146@gmail.com>
2026-05-08 18:28:11 +0530

    Add solution for Leetcode 141: Linked List Cycle

---
+++ src/leetcode/LC141.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LC141 {
+     /*
+        Leetcode 141. Linked List Cycle
+    */
+
+    static boolean hasCycle(ListNode head) {
+        if (head==null){
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if (slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+          ListNode head = new ListNode(3);
+          head.next = new ListNode(2);
+          head.next.next = new ListNode(0);
+          head.next.next.next = new ListNode(-4);
+          head.next.next.next.next = head.next;
+          System.out.println(hasCycle(head));
+    }
+}
