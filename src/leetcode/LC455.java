1253bd87450e98cc72344c06260845280ca0669d
“Harsh6575” <harsh.190410107146@gmail.com>
2026-05-28 20:57:18 +0530

    Add solution for Leetcode 455: Assign Cookies

---
+++ src/leetcode/LC455.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC455 {
+     /*
+        Leetcode 455. Assign Cookies
+        Array and Two Pointers
+    */
+
+    static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findContentChildren(new int[]{1, 2, 3}, new int[]{1, 1, 1})); // 1
+        System.out.println(findContentChildren(new int[]{1, 2}, new int[]{1, 1, 1})); // 1
+        System.out.println(findContentChildren(new int[]{1, 2, 3}, new int[]{1, 2, 3})); // 3
+    }
+}
