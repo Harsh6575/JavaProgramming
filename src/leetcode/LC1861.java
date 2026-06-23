97fffee4f0cfb234d495c329fd2e1ef4d615f51e
“Harsh6575” <harsh.190410107146@gmail.com>
2026-06-23 08:07:32 +0530

    Add solution for Leetcode 1861: Rotate the Box

---
+++ src/leetcode/LC1861.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LC1861 {
+    /*
+        Leetcode 1861. Rotate the Box
+    */
+
+    static char[][] rotateTheBox(char[][] boxGrid) {
+        int rows = boxGrid.length;
+        int cols = boxGrid[0].length;
+        char[][] res = new char[cols][rows];
+        for (int r = 0; r < cols; r++)
+            for (int c = 0; c < rows; c++)
+                res[r][c] = '.';
+        
+        for (int r = 0; r < rows; r++) {
+            int p = cols - 1;
+            for (int c = cols - 1; c >= 0; c--) {
+                if (boxGrid[r][c] == '*') {
+                    res[c][rows - 1 - r] = '*';
+                    p = c - 1;
+                } else if (boxGrid[r][c] == '#')
+                    res[p--][rows - 1 - r] = '#';
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(rotateTheBox(new char[][]{{'#','.','#'}}))); // [["."],["#"],["#"]]
+        System.out.println(Arrays.deepToString(rotateTheBox(new char[][]{{'#','.','*','.'},{'#','#','*','.'}}))); // [["#","."],["#","#"],["*","*"],[".","."]]
+    }
+}
