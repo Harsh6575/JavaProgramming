06afc062c42e39d584240bbc8263ef2a3fdd625e
“Harsh6575” <harsh.190410107146@gmail.com>
2026-07-17 08:04:04 +0530

    Add solution for Leetcode 88: Merge Sorted Array

---
+++ src/leetcode/LC88.py
@@ -0,0 +1,20 @@
+# Leetcode 88: Merge Sorted Array
+
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        i, j, k = m - 1, n - 1, m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums1 = [1, 2, 3, 0, 0, 0]
+    nums2 = [2, 5, 6]
+    solution.merge(nums1, 3, nums2, 3)
+    print(nums1)  # Output: [1, 2, 2, 3, 5, 6]
