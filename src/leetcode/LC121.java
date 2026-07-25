02417f7f545daf1e891e75ce6c0064e890552e8a
“Harsh6575” <harsh.190410107146@gmail.com>
2026-07-25 09:17:24 +0530

    Add solution for Leetcode 121: Best Time to Buy and Sell Stock

---
+++ src/leetcode/LC121.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class LC121 {
+     /*
+        Leetcode 121. Best Time to Buy and Sell Stock
+    */
+
+    static  int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    }
+}
