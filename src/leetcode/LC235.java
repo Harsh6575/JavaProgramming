ee5bdb10a2196dbecf9c3cc3fbdfcd13ab7b318c
“Harsh6575” <harsh.190410107146@gmail.com>
2026-08-11 08:00:00 +0530

    Add solution for Leetcode 235: Lowest Common Ancestor of a Binary Search Tree

---
+++ src/leetcode/LC235.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class LC235 {
+     /*
+        Leetcode 235. Lowest Common Ancestor of a Binary Search Tree
+        Tree
+    */
+
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if (root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(7);
+        root.right.right = new TreeNode(9);
+        root.left.right.left = new TreeNode(3);
+        root.left.right.right = new TreeNode(5);
+
+        TreeNode ans = lowestCommonAncestor(root, root.left, root.right);
+        System.out.println(ans.val);
+    }
+}
