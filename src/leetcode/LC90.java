8ccfa37946b861705213d911cde7005513f91f92
Harsh6575 <harshvansjaliya3@gmail.com>
2026-08-19 08:20:19 +0530

    Add solution for Leetcode 90: Subsets II

---
+++ src/leetcode/LC90.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC90 {
+    /*
+        Leetcode 90. Subsets II
+        Recursion
+    */
+    static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    static void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            current.add(nums[i]);
+            backtrack(nums, i + 1, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(subsetsWithDup(new int[]{1,2,2}));
+    }
+}
